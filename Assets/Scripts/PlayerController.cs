7f23344b6bb6b1926b8ea2f7c77e247fdfd19207
caveiramica14 <micaeltiago2006@gmail.com>
2026-03-03 23:43:58 +0000

    2.2 passo 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public GameObject ProjectilePrefab;
     void Start()
     {
         
