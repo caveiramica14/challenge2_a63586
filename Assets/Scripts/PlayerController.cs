fc8fbd09c18854b224b39d4d56df7a7f86487cc8
caveiramica14 <micaeltiago2006@gmail.com>
2026-03-05 20:01:55 +0000

    Bonus Passo 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public float zRangeDown = - 2.0f;
+    public float zRangeUp = 7.0f;
     
     public GameObject ProjectilePrefab;
     void Start()
@@ -24,8 +27,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < zRangeDown)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeDown);
+        }
+        if (transform.position.z > zRangeUp)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeUp);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
